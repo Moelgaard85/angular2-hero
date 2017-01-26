4c543b85551ab0f6255f1f4b28ca06d00da459ef
Bjarne <bgp@proreact.dk>
2017-01-26 16:17:30 +0100

    Dev: Using promise in a service to be called from components

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,22 +1,26 @@
-import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-import { FormsModule } from '@angular/forms';
+import { BrowserModule } from '@angular/platform-browser';
+import { FormsModule} from '@angular/forms';
 import { HttpModule } from '@angular/http';
 
 import { AppComponent } from './app.component';
 import { HeroDetailComponent } from './hero-detail/hero-detail.component';
 
+import { HeroService } from './hero.service';
+
 @NgModule({
-  declarations: [
-    AppComponent,
-    HeroDetailComponent
-  ],
   imports: [
-    BrowserModule,
-    FormsModule,
-    HttpModule
+  BrowserModule,
+  FormsModule,
+  HttpModule
   ],
-  providers: [],
+  declarations: [
+  AppComponent,
+  HeroDetailComponent
+  ],
+  providers: [
+  HeroService
+  ],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
